abfc754a8d66aec64bda1b39cc4a03284a0da95a
soretaka <yatesthap@gmail.com>
2023-02-26 20:37:45 +0700

    select finished

--- 01/client/client_select.py
+++ 01/client/client_select.py
@@ -1,19 +1,87 @@
 import socket
 import sys
+from time import sleep
+import os
+from os import path
+import tqdm
 
-server_address = ('127.0.0.1', 5001)
+def get_string_between(str, sep1, sep2):
+    result = ""
+    try:
+        result = str.split(sep1)[1].split(sep2)[0]
+    except:
+        None
+    return result
+
+
+BUFFER_SIZE = 1024
+HOST = sys.argv[1]
+PORT = int(sys.argv[2])
+
+server_address = (HOST, PORT)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print(f"[+] Connecting to {HOST}:{PORT}")
 client_socket.connect(server_address)
-
-sys.stdout.write('>> ')
+print("[+] Connected.")
+print("[SEND] >> ")
 
 try:
     while True:
         message = sys.stdin.readline()
         client_socket.send(bytes(message, 'utf-8'))
-        received_data = client_socket.recv(1024).decode('utf-8')
-        sys.stdout.write(received_data)
-        sys.stdout.write('>> ')
+        received_data = client_socket.recv(BUFFER_SIZE).decode('utf-8')
+
+        # Special handling if we send unduh message
+        if message.split(" ")[0] == "download":
+            # check the confirmation is the file exist
+            if received_data == "CONFIRMATION::FILE_EXIST\n":
+                # read the header message
+                received_header = client_socket.recv(BUFFER_SIZE)
+                print(f"received_header : [{received_header}]")
+                received_header = received_header.decode('utf-8')
+                recv_filename = get_string_between(received_header, "file-name: ", "\n")
+                recv_filesize = int(get_string_between(received_header, "file-size: ", "\n"))
+                
+                print(f"We got header ::\nFile Name : {recv_filename}\nFile Size: {recv_filesize}")
+                # delete the file first,
+                try:
+                    os.remove(recv_filename)
+                except:
+                    None
+
+                #read the file content
+                #setup the progress bar
+                total_data_recv = 0
+                with open(recv_filename, "wb") as f:
+                    while True:
+                        # read %BUFFER_SIZE% bytes from the socket (receive)
+                        bytes_read = client_socket.recv(BUFFER_SIZE)
+                        # print(bytes_read)
+
+                        if not bytes_read:    
+                            # nothing is received
+                            # file transmitting is done
+                            break
+                        # write to the file the bytes we just received
+                        f.write(bytes_read)
+                        total_data_recv += len(bytes_read)
+                        #update the progress status
+                        print(f"[!] File transfer {recv_filename} : {total_data_recv}B/{recv_filesize}B ({(total_data_recv/recv_filesize)*100}%)                 \r", end="")
+
+                        # check is the file transfer completed
+                        if path.getsize(recv_filename) == recv_filesize:
+                            sys.stdout.write("\n[!] File transfer done ~\n[SEND] >> ")
+                            break
+            else:
+                sys.stdout.write(f"[RECV] << {received_data}[SEND] >> ")
+        #special handling for exit
+        elif f"{message.rstrip()} ".split(" ")[0] == "exit":
+            client_socket.send(bytes("REQUEST::DISCONNECT", 'utf-8'))
+            client_socket.close()
+            sys.stdout.write(f"[!] Bye")
+            break
+        else:
+            sys.stdout.write(f"[RECV] << {received_data}[SEND] >> ")
 
 except KeyboardInterrupt:
     client_socket.close()
--- 01/server/server_select.py
+++ 01/server/server_select.py
@@ -1,8 +1,15 @@
 import socket
 import select
 import sys
+import os
+from os import path
 
-server_address = ('127.0.0.1', 5001)
+BUFFER_SIZE = 1024
+HOST = "localhost"
+PORT = 5000
+
+server_address = (HOST, PORT)
+print(f"[+] Listening from {HOST}:{PORT}")
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind(server_address)
@@ -17,18 +24,60 @@ try:
         for sock in read_ready:
             if sock == server_socket:
                 client_socket, client_address = server_socket.accept()
-                input_socket.append(client_socket)        
-            
+                print(f"[+] New client {client_address} is connected.")
+                input_socket.append(client_socket)
             else:            	
-                data = sock.recv(1024)
-                print(sock.getpeername(), data)
-            
-                if data:
-                    sock.send(data)
-                else:                    
+                data = sock.recv(BUFFER_SIZE)
+                print(f"[!] {sock.getpeername()} << {data.decode().rstrip()}")
+
+                data_split = f"{data.decode().rstrip()} ".split(" ")
+
+                if data_split[0] == "download":
+                    # check if file exist
+                    filename = data.decode().split("download ")[1].rstrip()
+                    if path.exists(f"files/files/{filename}"):
+                        sock.send(bytes("CONFIRMATION::FILE_EXIST\n", 'utf-8'))                        
+                        filesize = os.path.getsize(f"files/files/{filename}")
+
+                        # initiate headers
+                        header = f"file-name: {filename}\n"
+                        header += f"file-size: {filesize}\n\n\n"
+                        
+                        sock.send(bytes(header, 'utf-8'))
+
+                        # start sending the file
+                        total_data_sent = 0
+                        with open(f"files/files/{filename}", "rb") as f:
+                            while True:
+                                # read the bytes from the file
+                                bytes_read = f.read(BUFFER_SIZE)
+                                total_data_sent += len(bytes_read)
+                                #update the progress status
+                                print(f"[!] File transfer {filename} : {total_data_sent}B/{filesize}B ({int((total_data_sent/filesize)*100)}%)                 \r", end="")
+
+                                if not bytes_read:
+                                    print("[!] Transfer fle is done!")
+                                    # file transmitting is done
+                                    break
+
+                                # send to client
+                                sock.sendall(bytes_read)
+                    else:
+                        sock.send(bytes("filenya gaada bos\n", 'utf-8'))
+                elif data_split[0] == "REQUEST::DISCONNECT":
+                    print(f"[-] Client {sock.getpeername()} just disconnected.")
                     sock.close()
                     input_socket.remove(sock)
+                    None
+                else:
+                    if data:
+                        sock.send(data)
+                    else:
+                        print(f"[-] Client {sock.getpeername()} just disconnected.")
+                        sock.close()
+                        input_socket.remove(sock)
+            
 
-except KeyboardInterrupt:        
+except KeyboardInterrupt:
     server_socket.close()
     sys.exit(0)
